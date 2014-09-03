5c29ed4f9a3e1d6b4d0438853f6c5234d14e9e20
Jeff Kingyens <jeff.kingyens@gmail.com>
2014-09-02 18:27:56 -0700

    add logging

--- watch.js
+++ watch.js
@@ -1,47 +1,54 @@
 var async = require('async');
-var chokidar = require('chokidar'); 
+var chokidar = require('chokidar');
 var cp = require('child_process');
 
 var docker_ip;
 
-function getdockerip (cb) { 
-  cp.exec('boot2docker ip 2>/dev/null', function (err, stdout) { 
-    if (err) { 
+function getdockerip (cb) {
+  cp.exec('boot2docker ip 2>/dev/null', function (err, stdout) {
+    if (err) {
       return cb(err);
     }
-    docker_ip = stdout; 
+    docker_ip = stdout;
     cb();
   });
 }
 
-function install_rsync (cb) { 
-  cp.exec('boot2docker ssh "tce-load -wi rsync"', function (err, stdout) { 
+function install_rsync (cb) {
+  cp.exec('boot2docker ssh "tce-load -wi rsync"', function (err, stdout) {
     cb();
   });
-} 
+}
 
-function rsync (cb) { 
-  var child = cp.spawn('rsync', [ 
+function rsync (cb) {
+  var child = cp.spawn('rsync', [
     '-av',
      process.cwd() + '/',
      'docker@' + docker_ip + ':' + process.cwd()
   ]);
-  child.on('exit', cb);
+  child.stdout.on('data', function (data) {
+    console.log(data.toString());
+  });
+  child.on('exit', function () {
+    cb();
+  });
 }
 
-function mkdirp (cb) { 
-  cp.exec('boot2docker ssh "sudo mkdir -p ' + process.cwd() + ' && sudo chown docker ' + process.cwd() + '"', cb);
+function mkdirp (cb) {
+  cp.exec('boot2docker ssh "sudo mkdir -p ' + process.cwd() + ' && sudo chown docker ' + process.cwd() + '"', function() {
+    cb();
+  });
 }
 
-async.series([ 
+async.series([
   mkdirp,
   getdockerip,
   install_rsync,
   rsync
-], function (err) { 
+], function (err) {
   var watcher = chokidar.watch(process.cwd(), { persistent: true });
-  watcher.on('change', function () { 
-    rsync(function (cb) { 
+  watcher.on('change', function () {
+    rsync(function (cb) {
       console.log('refreshing');
     });
   });
